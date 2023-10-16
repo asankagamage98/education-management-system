3f94ee91b210306399f290c92021454811a22052
asankagamage98 <asankagamage1998@gmail.com>
2023-10-16 20:34:23 +0530

    created EventController & crete table

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function store(Request $request){
+        $fillable = $request->validate([
+            'name'=>'required|string',
+            'venue'=>'required|string',
+            'startDate'=>'required|string',
+            'startTime'=>'required|string',
+            'Organizer'=>'required|string',
+            'cost'=>'required|string'
+        ]);
+
+        return Event::create( $fillable)->all();
+    }
+
+    public function getall(){
+        return Event::all();
+    }
+    public function getById(string $id){
+        $findData = Event::find($id);
+        return $findData;
+
+    }
+    public function update(Request $request, string $id){
+        $data = Event::find($id);
+        $data->update($request->all());
+        return $data;
+    }
+    public function search(string $name){
+        return Event::where('name','like','%'.name.'%')->get();
+
+    }
+    public function delete(string $id){
+        Event::destroy($id);
+        return Event::all();
+    }
+}
---
+++ database/migrations/2023_10_16_144102_create_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('venue');
+            $table->string('startDate');
+            $table->string('startTime');
+            $table->string('Organizer');
+            $table->string('cost');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
